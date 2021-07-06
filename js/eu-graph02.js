edf4bfc4c2a43513798e2e9cfb21b6a260ff2ecb
Mth Lht <mathieu.lehot@protonmail.com>
2021-07-06 18:18:24 +0200

    'new euro map'

--- js/eu-graph02.js
+++ js/eu-graph02.js
@@ -1,204 +1,261 @@
 Promise.all([
     d3.json("data/ftv_eu.geojson"),
-    d3.csv("data/owid_incid.csv")
+    d3.csv("data/owid_incid_evol.csv")
 ]).then(showData);
 
 function showData(data) {
     const graphCfg = {
         target: `#eu-graph02`,
-        title: `Nombre de cas Covid-19 par pays en Europe`,
-        subtitle: `en nombre de cas, lissés sur une semaine, pour un million d'habitants [[autoDate]]`,
+        title: `Variation des nouveaux cas Covid-19 par pays en Europe`,
+        //subtitle: `entre le XX mois XXXX et le XX mois XXXX`,
         caption: `Source. <a href='https://ourworldindata.org/coronavirus' target='_blank'>Our world in data</a>`,
         type: 'landscape', // définition du format du graphe
         device: window.screenDevice, // récupération de la largeur de l'écran
     }
 
 
-// Tri des données
+    // Tri des données
 
-// données carto
-let dataMap = data[0];
+    // données carto
+    let dataMap = data[0];
 
-// données taux de vaccination
-let dataVacc = data[1];
+    // données taux de vaccination
+    let dataIncid = data[1];
 
-// création d'un container pour le tri des données de vaccination
-let dataContainer = {
-    new_cases_smoothed_per_million: {},
-    date: {}
-};
+    // création d'un container pour le tri des données de vaccination
+    let dataContainer = {
+        incid_evol: {},
+        incid_diff: {},
+        date: {}
+    };
 
-// répartition des données d'incidence dans le container
-for (let d of dataVacc) {
+    // répartition des données d'incidence dans le container
+    for (let d of dataIncid) {
 
-    let code_pays = d.iso_code;
+        let code_pays = d.iso_code;
 
-    dataContainer.new_cases_smoothed_per_million[code_pays] = d.new_cases_smoothed_per_million;
-    dataContainer.date[code_pays] = d.date;
+        dataContainer.incid_evol[code_pays] = d.incid_evol;
+        dataContainer.incid_diff[code_pays] = d.diff;
+        dataContainer.date[code_pays] = d.date;
 
-}
+    }
 
-// répartition des données d'incidence dans les properties des polygones de la carte
-dataMap.features = dataMap.features.map((d) => {
+    // répartition des données d'incidence dans les properties des polygones de la carte
+    dataMap.features = dataMap.features.map((d) => {
 
-    let code_pays = d.properties.iso_a3;
+        let code_pays = d.properties.iso_a3;
 
-    d.properties.new_cases_smoothed_per_million = +dataContainer.new_cases_smoothed_per_million[code_pays]; // ATTENTION STRING A TRANSPOSER EN FLOAT
-    d.properties.date = new Date(dataContainer.date[code_pays]); // ATTENTION À TRANSPOSER EN FORMAT DATE
+        d.properties.incid_evol = +dataContainer.incid_evol[code_pays] / 100; // ATTENTION STRING A TRANSPOSER EN FLOAT
+        d.properties.incid_diff = +dataContainer.incid_diff[code_pays]; // ATTENTION STRING A TRANSPOSER EN FLOAT
+        d.properties.date = new Date(dataContainer.date[code_pays]); // ATTENTION À TRANSPOSER EN FORMAT DATE
 
-    return d;
+        return d;
 
-});
+    });
 
-//---------------------------------------------------------------------------------------
+    //---------------------------------------------------------------------------------------
 
-// Création du canevas SVG
+    // Création du canevas SVG
 
-const width = 500;
-const height = 300;
-const marginH = 80;
-const marginV = 20;
-const leg = 20;
+    const width = 500;
+    const height = 300;
+    const marginH = 80;
+    const marginV = 20;
+    const leg = 20;
 
-const viewBox = {
-    width: width + marginH * 2,
-    height: height + leg + marginV * 2
-};
+    const viewBox = {
+        width: width + marginH * 2,
+        height: height + leg + marginV * 2
+    };
 
-// création du canevas pour le Graphique
-const svg = d3
-    .select(graphCfg.target)
-    .select('.grph-content')
-    .insert('svg', ':first-child')
-    .attr("viewBox", [0, 0, viewBox.width, viewBox.height])
-    .attr("preserveAspectRatio", "xMinYMid");
+    // création du canevas pour le Graphique
+    const svg = d3
+        .select(graphCfg.target)
+        .select('.grph-content')
+        .insert('svg', ':first-child')
+        .attr("viewBox", [0, 0, viewBox.width, viewBox.height])
+        .attr("preserveAspectRatio", "xMinYMid");
 
-// création d'un groupe g pour la Légende
-const svgLegend = svg
-    .append("g")
-    .attr("transform", `translate(${marginH}, ${marginV})`);
+    // création d'un groupe g pour la Légende
+    const svgLegend = svg
+        .append("g")
+        .attr("transform", `translate(${marginH}, ${marginV})`);
 
-// création d'un groupe g pour le Graphique
-const svgPlot = svg
-    .append("g")
-    .attr("transform", `translate(${marginH}, ${marginV + leg})`);
+    // création d'un groupe g pour le Graphique
+    const svgPlot = svg
+        .append("g")
+        .attr("transform", `translate(${marginH}, ${marginV + leg})`);
 
-//---------------------------------------------------------------------------------------
+    //---------------------------------------------------------------------------------------
 
-// Date à afficher dans le titre
-// ATTENTION CETTE DATE DOIT FORCÉMENT ÊTRE PRISE DANS LE DATASET DU TAUX D'INCIDENCE
-const formatTimeToTitle = d3.timeFormat("%d %b %Y");
-const actualDate = new Date(dataVacc[0].date);
-const dateToTitle = formatTimeToTitle(actualDate);
+    // Écriture titraille
 
-// Écriture titraille graphique
+    // Définition du padding à appliquer aux titres, sous-titres, source
+    // pour une titraille toujours alignée avec le graphique
+    const paddingTxt = `0 ${marginH / viewBox.width * 100}%`
 
-// Définition du padding à appliquer aux titres, sous-titres, source
-// pour une titraille toujours alignée avec le graphique
-const paddingTxt = `0 ${marginH / viewBox.width * 100}%`
+    // Écriture du titre
+    d3.select(graphCfg.target)
+        .select('.grph-title')
+        .html(graphCfg.title)
+        .style("padding", paddingTxt);
 
-// Écriture du titre
-d3.select(graphCfg.target)
-    .select('.grph-title')
-    .html(graphCfg.title)
-    .style("padding", paddingTxt);
+    // Date à afficher dans le titre
+    // ATTENTION CETTE DATE DOIT FORCÉMENT ÊTRE PRISE DANS LE DATASET DU TAUX D'INCIDENCE
+    const formatTimeToTitle = d3.timeFormat("%d %b %Y");
+    const actualDate = new Date(dataIncid[0].date);
+    // Soustraction de 7 jours à la date (attention modification de l'objet en place)
+    const formerDate = actualDate.setDate(actualDate.getDate() - 7);
+    // Rajout des 7 jours à l'objet modifié en place
+    actualDate.setDate(actualDate.getDate() + 7);
 
-// Écriture du sous-titre
-d3.select(graphCfg.target)
-    .select('.grph-subtitle')
-    .html(graphCfg.subtitle.replace(/\[\[\s*autoDate\s*\]\]/, `${dateToTitle}`))
-    .style("padding", paddingTxt);
+    // Formatage des dates à afficher
+    const actualDateToTitle = formatTimeToTitle(actualDate);
+    const formerDateToTitle = formatTimeToTitle(formerDate);
 
-// Écriture de la source
-d3.select(graphCfg.target)
-    .select('.grph-caption')
-    .html(graphCfg.caption)
-    .style("padding", paddingTxt);
+    // Écriture du sous-titre
+    d3.select(graphCfg.target)
+        .select('.grph-title')
+        .append('span')
+        .attr('class', 'grph-date')
+        .html("entre le " + formerDateToTitle + " et le " + actualDateToTitle)
 
-//---------------------------------------------------------------------------------------
+    // Écriture de la source
+    d3.select(graphCfg.target)
+        .select('.grph-caption')
+        .html(graphCfg.caption)
+        .style("padding", paddingTxt);
 
-// Création de l'échelle de couleur
+    //---------------------------------------------------------------------------------------
 
-// échelle de couleur
-const seqScale = d3.scaleLinear()
-    .domain([0, 0.5, 2.5, 5, 10, 50, 100, 500, 1000, 4000])
-    .range(['#fff5f0', '#fee0d2', '#fcbba1', '#fc9272', '#fb6a4a', '#ef3b2c', '#cb181d', '#a50f15', '#67000d', '#011615']);
+    // Création de l'échelle de couleur
+
+    // Création de l'échelle de couleur
+
+    // Fonction génératrice de l'échelle
+
+    // définition d'une variable délimitant les 3/4 des valeurs
+    const divScale = d3
+        .scaleDiverging((t) => d3.interpolateRdBu(1 - t))
+        .domain([
+            -1,
+            0,
+            1
+        ]);
+
+    //---------------------------------------------------------------------------------------
+
+    // Projection carte
+
+    // définition de la projection de la carte (en geoNaturalEarth1)
+    const projection = d3.geoNaturalEarth1()
+        .center([15, 54])
+        //.scale([width / (1.3 * Math.PI)])
+        .translate([width / 2, height / 2])
+        .scale([width / 1.1]);
+
+    // création d'un générateur géographique de formes
+    const path = d3.geoPath().projection(projection);
+
+    // création d'un groupe g par polygone
+    const polygons = svgPlot
+        .selectAll("g")
+        .data(dataMap.features)
+        .join("g")
+
+    // projection des polygones géographiques
+    polygons
+        .append("path")
+        .attr("d", (d) => path(d))
+        .attr("stroke", "#ffffff")
+        .attr("fill", (d) => d.properties.incid_evol ? divScale(d.properties.incid_evol) : "#eee")
+        .style("stroke-width", "0.5px");
 
 
-//---------------------------------------------------------------------------------------
 
-// Projection carte
+    //---------------------------------------------------------------------------------------
 
-// définition de la projection de la carte (en geoNaturalEarth1)
-const projection = d3.geoNaturalEarth1()
-    .center([15, 54])
-    //.scale([width / (1.3 * Math.PI)])
-    .translate([width / 2, height / 2])
-    .scale([width / 1.1]);
+    // Legende ---- fonctionne avec l'API d3-legend
+    // https://d3-legend.susielu.com/
 
-// création d'un générateur géographique de formes
-const path = d3.geoPath().projection(projection);
+    // fonction génératrice de limites par dizaines
+    function range10s(start, end, diviseur) {
+        let container = [];
 
-// création d'un groupe g par polygone
-const polygons = svgPlot
-    .selectAll("g")
-    .data(dataMap.features)
-    .join("g")
-// projection des polygones géographiques
-polygons
-    .append("path")
-    .attr("d", (d) => path(d))
-    .attr("stroke", "#ffffff")
-    .attr("fill", (d) => d.properties.new_cases_smoothed_per_million ? seqScale(d.properties.new_cases_smoothed_per_million) : "#eee")
-    .style("stroke-width", "0.5px");
+        const start10s = Math.round(start / diviseur) * diviseur;
+        const end10s = Math.round(end / diviseur) * diviseur;
 
+        for (let i = start10s; i <= end10s; i += diviseur) {
+            container.push(i);
+        }
 
+        return container;
+    }
 
-//---------------------------------------------------------------------------------------
+    // Stockage des valeurs minimum et maximum de l'évolution
+    minIncidEvol = d3.min(dataIncid, (d) => +d.incid_evol);
+    maxIncidEvol = d3.max(dataIncid, (d) => +d.incid_evol);
 
-// Legende ---- fonctionne avec l'API d3-legend
-// https://d3-legend.susielu.com/
+    // Définition de de la liste des valeurs (legCells) qui seront affichées dans la légende
+    // Dans un premier temps la fonction range10s est lancé avec 10 comme diviseur
+    let legCells = range10s(minIncidEvol, maxIncidEvol, 10).map((d) => d / 100);
 
-// paramètres de la legende à l'aide de la variable legCells définie avec l'échelled de couleur
-const legend = d3
-    .legendColor()
-    .shapeWidth(width / 10)
-    .shapeHeight(10)
-    .cells([0, 0.5, 2.5, 5, 10, 50, 100, 500, 1000, 4000])
-    .orient("horizontal")
-    .labelAlign("middle")
-    .scale(seqScale);
+    // Si le diviseur par 10 renvoie un trop grand nombre de valeurs alors calcule une nouvelle répartition
+    let i = 10;
 
-// projection de la légende
-svgLegend.call(legend)
-    .selectAll("text")
-    .attr("fill", "grey")
-    .attr("font-size", "12px");
+    while (legCells.length > 10) {
+        i = i + 10
+        legCells = range10s(minIncidEvol, maxIncidEvol, i).map((d) => d / 100);
+    };
 
-//---------------------------------------------------------------------------------------
+    // paramètres de la legende à l'aide de la variable legCells définie avec l'échelled de couleur
+    const legend = d3
+        .legendColor()
+        .shapeWidth(width / 9)
+        .labelFormat(d3.format(".0%"))
+        .cells([-1, -0.75, -0.5, -0.25, 0, 0.25, 0.5, 0.75, 1])
+        .orient("horizontal")
+        .scale(divScale);
 
-// Animation carte
+    // projection de la légende
+    svgLegend.call(legend)
+        .selectAll("text")
+        .attr("fill", "grey")
+        .attr("font-size", "12px");
 
-// Animation carte
+    //---------------------------------------------------------------------------------------
 
-// création d'un groupe g qui contiendra le tooltip de la légende
-const tooltip = svgPlot.append("g")
-    .attr("transform", `translate(${0}, ${height / 1.4})`);
+    // Animation carte
 
-// condition pour que l'animation ne fonctionne que sur desktop
-// voir script device_detector pour la fonction deviceType()
-if (graphCfg.device !== 'mobile') {
-    // création du tooltip de la légende personnalisé
-    const custTooltip = commonGraph.tooltip(graphCfg.target, d3)
+    // création d'un groupe g qui contiendra le tooltip de la légende
+    const tooltip = svgPlot.append("g")
+        .attr("transform", `translate(${0}, ${height / 1.4})`);
 
-    polygons.on("mouseover", function (d, idx, arr) {
-        // lors du survol avec la souris l'opacité des barres passe à 1
-        d3.select(this).attr("opacity", 0.6);
+    if (graphCfg.device !== 'mobile') {
+            // création du tooltip de la légende personnalisé
+            const custTooltip = commonGraph.tooltip(graphCfg.target, d3)
+
+    polygons.on("mouseover", function (d) {
+        // lors du survol avec la souris l'opacité des polygones passe à 0.8
+        d3.select(this).attr("opacity", 0.8);
 
         // format de la date affichée dans le tooltip
         // stockage de la date de la barre survolée au format XX mois XXXX dans une variable
-        const formatTime = d3.timeFormat("%d %b %Y");
-        const instantT = formatTime(d.date);
+        const formatTime = d3.timeFormat("%d %b");
+        let dateT = d.properties.date;
+        let instantT = formatTime(dateT);
+
+        // ON ENLÈVE 7 JOURS À LA DATE - ATTENTION car .setDate() modifie l'objet en place
+        let instantT7 = formatTime(dateT.setDate(dateT.getDate() - 7));
+
+        // ATTENTION À BIEN RAJOUTER LES 7 JOURS à dateT
+        dateT.setDate(dateT.getDate() + 7);
+
+        // variation ou baisse selon la valeur incid_evol
+        let variation = +d.properties.incid_evol > 0 ? "hausse" : "baisse";
+
+        // valeur arrondie à 2 décimales de incid_evol
+        let valeur = Math.abs(+d.properties.incid_evol * 100).toFixed(2).replace('.00', '').replace('.', ','); // Remplace le point en virgule et supprime les décimales nulles.
 
         // efface les données du tooltip
         custTooltip.html('')
@@ -214,18 +271,18 @@ if (graphCfg.device !== 'mobile') {
 
         custTooltip
             .append('div')
-            .html(`${Math.round(d.properties.new_cases_smoothed_per_million).toLocaleString("fr-FR")} cas pour un million d'habitants`);
+            .html(`en ${variation} de ${valeur}% sur une semaine`);
+
 
     });
 
     // efface le contenu du groupe g lorsque la souris ne survole plus le polygone
     polygons.on("mouseout", function () {
-
         d3.select(this).attr("opacity", 1); // rétablit l'opacité à 1
 
-        custTooltip.style('opacity', '0')
-
+        custTooltip.style('opacity', '0');
     });
+
+};
+
 }
-  
-}  
