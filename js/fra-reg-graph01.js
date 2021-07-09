d2ad4823dcade552aea3c1edd2bc4d616daa286c
Mth Lht <mathieu.lehot@protonmail.com>
2021-07-09 17:41:55 +0200

    titraille graphs vaccins & nudge carte France

--- js/fra-reg-graph01.js
+++ js/fra-reg-graph01.js
@@ -276,7 +276,7 @@ Promise.all([
     // ATTENTION À BIEN RAJOUTER LES 7 JOURS à dateT
     dateT.setDate(dateT.getDate() + 7);
 
-    // Affichage du nom du département en gras
+    // Efface le nudge
     tooltip
       .selectAll('text')
       .remove()
@@ -284,7 +284,8 @@ Promise.all([
     tooltip
       .selectAll('path')
       .remove()
-
+    
+    // Affichage du nom du département en gras
     tooltip
       .append("text")
       .attr("y", 0)
